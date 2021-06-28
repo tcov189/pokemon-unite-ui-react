82032ce16c50d7295ed51cd67cde9f84056fe660
Trevor Covington <tcovington189@gmail.com>
2021-06-28 13:59:52 -0500

    Created basic wrapping components (#3)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import Header from './Layout/Header'
+import Content from './Layout/Content'
+import Footer from './Layout/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-300 min-h-screen flex flex-col justify-between antialiased">
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="flex py-2 px-2 mb-2 bg-gray-600 text-gray-200 text-xl">
+            <div className="pr-5">
+                Nav
+            </div>
+            <div>
+                Pokemon
+            </div>
+        </header>
+    )
+}
+
+export default Header
