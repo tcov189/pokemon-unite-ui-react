48086483306ee33668aff04beaf05ee6fc4bb7a9
Trevor Covington <tcovington189@gmail.com>
2021-06-28 13:59:32 -0500

    Created basic wrapping components

---
+++ src/Layout/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Content() {
+    return (
+        <div className="px-2 flex-1">
+            This is where content will go
+        </div>
+    )
+}
+
+export default Content
---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="py-1 px-2 text-center bg-gray-600 text-gray-200 text-sm">
+            Pokémon images &amp; names &copy; 1995-2021 Nintendo/Game Freak.
+        </div>
+    )
+}
+
+export default Footer
