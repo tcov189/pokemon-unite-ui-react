23cbc35f460fe788bbafab7e6fdfd1c938883bb8
Trevor Covington <tcovington189@gmail.com>
2021-06-29 18:46:51 -0500

    Adding React Router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Header from './Layout/Header'
 import Footer from './Layout/Footer'
 import PokemonList from './Pages/PokemonList';
@@ -5,9 +7,15 @@ import PokemonList from './Pages/PokemonList';
 function App() {
   return (
     <div className="bg-gray-500 min-h-screen flex flex-col justify-between antialiased">
-      <Header />
-      <PokemonList />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <PokemonList />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
