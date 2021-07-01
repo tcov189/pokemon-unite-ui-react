8925875cef6fbcae1b20c4726d6267d43ee0a7cc
Trevor Covington <tcovington189@gmail.com>
2021-07-01 14:07:48 -0500

    Adding color if image is missing

--- src/Pages/PokemonDetails/PokemonDetailsMoves/MoveIcon.jsx
+++ src/Pages/PokemonDetails/PokemonDetailsMoves/MoveIcon.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 
 function MoveIcon({ moveName, moveIconPath, onClick = null, isActive = false }) {
     const backgroundStyle = {
-        backgroundImage: `url(${moveIconPath})`,
+        backgroundImage: moveIconPath ? `url(${moveIconPath})` : null,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         width: '60px',
         height: '60px',
-
     }
+
+    if (!moveIconPath) {
+        backgroundStyle.backgroundColor = '#6b7280';
+    }
+
     return (
         <div className={`cursor rounded-full ${isActive ? 'border-2 border-yellow-500' : null}`} style={backgroundStyle} onClick={onClick}></div>
     )
